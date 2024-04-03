57ccd6946ba062884e95e003302c9781b57c7250
mznrpatti <maiznerpatricia1028@gmail.com>
2024-04-03 23:11:21 +0200

    UserModel and UserMapperProfile

---
+++ RentACar/Repository/Mappings/UserMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RentACar.Models;
+using RentACar.Models.Entities;
+
+namespace RentACar.Repository.Mappings
+{
+    public class UserMapperProfile : Profile
+    {
+        public UserMapperProfile()
+        {
+            //User to UserModel
+            CreateMap<User, UserModel>();
+        }
+    }
+}
